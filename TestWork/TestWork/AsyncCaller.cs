09dbf658f900e7cb0672d83a12cc173eecf11fa7
SerKhar <104434450+SerKhar@users.noreply.github.com>
2022-08-24 10:52:44 +0400

    Добавил решения 2 данных задач

---
+++ TestWork/TestWork/AsyncCaller.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestWork
+{
+    public class AsyncCaller
+    {
+        private EventHandler handler;
+
+        public AsyncCaller(EventHandler handler)
+        {
+            handler = handler;
+        }
+
+        public bool Invoke(int milliseconds, object sender, EventArgs args)
+        {
+            var task = Task.Factory.StartNew(() => handler.Invoke(sender, args));
+
+            return task.Wait(milliseconds);
+        }
+
+    }
+}
---
+++ TestWork/TestWork/Server.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestWork
+{
+    internal static class Server
+    {      
+        private static int count = 0;
+        private static ReaderWriterLockSlim locker = new ReaderWriterLockSlim();
+        public static int GetCount()
+        {
+            locker.EnterReadLock();
+            try
+            {
+                return count;
+            }
+            finally
+            {
+                locker.ExitReadLock();
+            }
+        }
+        public static void AddToCount(int value)
+        {
+            locker.EnterWriteLock();
+            count += value;
+            locker.ExitWriteLock();
+        }
+    }
+}
